e318c72ee214f28693bc880f2bb3b2f9c14e1d69
Yxla Souza <62162242+Ysla-Souza@users.noreply.github.com>
2021-06-04 18:23:55 -0300

    Update Exercicio6.java

--- Exercicio6.java
+++ Exercicio6.java
@@ -7,15 +7,15 @@ public class Exercicio6 {
 
 	public static void main(String[] args) {
 		
-		//Fazer um programa que leia trÍs valores com ponto flutuante de dupla precis„o: A, B e C. Em seguida, calcule e
+		//Fazer um programa que leia tr√™s valores com ponto flutuante de dupla precis√£o: A, B e C. Em seguida, calcule e
 		//mostre:
-			//a) a ·rea do tri‚ngulo ret‚ngulo que tem A por base e C por altura.
-			//b) a ·rea do cÌrculo de raio C. (pi = 3.14159)
-			//c) a ·rea do trapÈzio que tem A e B por bases e C por altura.
-			//d) a ·rea do quadrado que tem lado B.
-			//e) a ·rea do ret‚ngulo que tem lados A e B.
+			//a) a √°rea do tri√¢ngulo ret√¢ngulo que tem A por base e C por altura.
+			//b) a √°rea do c√≠rculo de raio C. (pi = 3.14159)
+			//c) a √°rea do trap√©zio que tem A e B por bases e C por altura.
+			//d) a √°rea do quadrado que tem lado B.
+			//e) a √°rea do ret√¢ngulo que tem lados A e B.
 		Locale.setDefault(Locale.US);
-		Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		double a, b, c, triangulo, circulo, trapezio, quadrado,retangulo;
 		
@@ -33,7 +33,7 @@ public class Exercicio6 {
 		//e)
 		retangulo =  a  *  b ;
 		
-		System.out.printf("TRINGULO:%.3f%n" , triangulo);
+		System.out.printf("TRINGULO: %.3f%n" , triangulo);
 		System.out.printf("CIRCULO: %.3f%n" , circulo);
 		System.out.printf("TRAPEZIO: %.3f%n" , trapezio);
 		System.out.printf("QUADRADO: %.3f%n" , quadrado);
